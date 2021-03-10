9dad6179c7739f10a2090fec2f9bc9e6832ee4ca
alyallmann <77987536+alyallmann@users.noreply.github.com>
2021-03-10 10:25:52 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,19 @@
 using namespace std;
 
 int main()
+  string filename;
+string paceholder;
+fstream reader;
+
+//get use input...
+cout <<  "Please enter the file to be opened:\n";
+cin>>  filename;
+//try to open the file...
+reader.open(filename,ios::in);
+
+
+
+  
 {
 
   return 0;
