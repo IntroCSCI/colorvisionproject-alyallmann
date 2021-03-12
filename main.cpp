4f26bb48f71e7268d75e8f7aadf3ace8f6e7dd90
alyallmann <77987536+alyallmann@users.noreply.github.com>
2021-03-11 21:26:19 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,22 +1,44 @@
-//Author: PUT YOUR NAME HERE
+//Author: Alyssa Allmann
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 int main()
-  string filename;
-string paceholder;
-fstream reader;
-
-//get use input...
-cout <<  "Please enter the file to be opened:\n";
-cin>>  filename;
-//try to open the file...
-reader.open(filename,ios::in);
-
-
-
-  
 {
+string fileName="";
+ifstream reader;
+string line= "";
+size_t position;
+int amount;
 
+cout << "Please enter the file to be opened:\n";
+getline(cin,fileName);
+
+reader.open(fileName,ios::in);
+
+if(reader.is_open())
+{
+while( !reader.eof())
+{
+getline(reader,line);
+position = line.find("#"); // search within line for a pound sign
+// cout << position << endl;
+
+if(line[position]){
+for (int counter = position+=1; counter < line.size(); counter
+++)
+{
+  if(line [counter] >= '0'&& line[counter] <= '9' || line[counter] >= 'a' && line[counter] <= 'f' || line[counter] >= 'A' && line[counter] <= 'F'){
+  cout << line[counter];
+  int sixth=position+=5;
+
+  if( (sixth >= '0' && sixth <= '9') || (sixth >='a' && sixth <= 'f') || (sixth >= 'A' && sixth <='F'))
+  
+}  
+ }
+  }
+} 
+reader.close();
   return 0;
+}
 }
